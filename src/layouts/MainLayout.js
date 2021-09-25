f3227cd3f9be9dd1504575e376b4f10e67b45a1d
Jona <>
2021-09-25 12:53:06 -0400

    add login page

---
+++ src/layouts/MainLayout.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+const MainLayout = ({ children }) => {
+    return (
+     <>
+      <Navbar bg="light" variant="light" className="mb-4">
+        <Navbar.Brand href="/">Meeting Summarizer</Navbar.Brand>
+
+        <Nav>
+        
+        </Nav>
+
+      </Navbar>
+      <Container>
+        {children}
+      </Container>
+    </>
+    ) 
+}
+
+export default MainLayout
---
+++ src/pages/Login.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Button, Col, Form, Row, Card} from "react-bootstrap"
+import MainLayout from '../layouts/MainLayout'
+
+
+const Login = () => {
+    return (
+     <MainLayout>  
+     <Row className="justify-content-center">
+        <Col lg={6} md={8}>
+          <Card>
+            <Card.Body>
+              <Form.Group>
+                <Form.Label>Phone Number</Form.Label>
+                <Form.Control 
+                  type="text" 
+                  placeholder="Enter #" 
+                />
+              </Form.Group>
+              <br/> 
+              <Button className="btn-success" variant="standard" >
+                Go
+              </Button>
+            </Card.Body>
+          </Card>
+        </Col>
+
+      </Row>
+      </MainLayout> )
+} 
+
+
+export default Login;
