5bc080ecfa43c85bb8d7f998e5f740889841bf70
Massiel50 <massielescano@yahoo.com>
2021-09-25 13:56:41 -0400

    dash cards are now card with buttons within

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -9,19 +9,37 @@ function Dashboard () {
 
     {/* section to input phone number and submit button */}
     <Card>
-      <Card.Header as="h5">Give me your phone number</Card.Header>
+      <Card.Header as="h5">Give me your phone number sweet stuff</Card.Header>
       <Card.Body>
         <Form.Control type="text" placeholder="Readonly input here..." readOnly />
-        <Button variant="primary">Submit</Button>
+        <Button variant="primary" type= "submit">Submit</Button>
       </Card.Body>
     </Card>
 
 {/* section where a list of things are displayed */}
-<Card body>This is some text within a card body.</Card>
+<Card body>
+<div className="d-grid gap-2">
+  <Button variant="secondary" size="lg">
+    Some Phone number
+  </Button>
+</div>
+</Card>
 
-<Card body>This is some text within a card body.</Card>
+<Card body>
+<div className="d-grid gap-2">
+  <Button variant="secondary" size="lg">
+    Some Phone number
+  </Button>
+</div>
+</Card>
 
-<Card body>This is some text within a card body.</Card>
+<Card body>
+<div className="d-grid gap-2">
+  <Button variant="secondary" size="lg">
+    Some Phone number
+  </Button>
+</div>
+</Card>
   </div></>
   )
 };
