0c9d001adaf213e499835224d5296cf13185b7ae
Massiel50 <massielescano@yahoo.com>
2021-09-25 15:44:02 -0400

    login page made by Jona

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React from 'react'
+import { Button, Col, Form, Row, Card} from "react-bootstrap"
+import MainLayout from '../layouts/MainLayout'
+
+
 const Login = () => {
-    return(
-        <div>
-            <h1>Login</h1>
-        </div>
-    )
-}
+    return (
+     <MainLayout>  
+     <Row className="justify-content-center">
+        <Col lg={6} md={8}>
+          <Card>
+            <Card.Body>
+              <Form.Group>
+                <Form.Label>Phone Number</Form.Label>
+                <Form.Control 
+                  type="text" 
+                  placeholder="Enter #" 
+                />
+              </Form.Group>
+              <br/> 
+              <Button className="btn-success" variant="standard" >
+                Go
+              </Button>
+            </Card.Body>
+          </Card>
+        </Col>
+
+      </Row>
+      </MainLayout> )
+} 
 
 export default Login;
