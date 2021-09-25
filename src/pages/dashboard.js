d62a9af1d9404e7717fd1099d24d14aa496555fb
Massiel50 <massielescano@yahoo.com>
2021-09-25 12:13:55 -0400

    dashboard card, input and button

---
+++ src/pages/dashboard.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const dashboard = props => (
+  <><div className="dashcard" /><div>
+    <Card body>
+    <Form.Control type="text" placeholder="Readonly input here..." readOnly />
+    <>
+  <Button variant="primary" size="lg" active>
+    Primary button
+  </Button>{' '}
+  <Button variant="secondary" size="lg" active>
+    Button
+  </Button>
+</>
+    </Card>
+  </div></>
+);
+
+export default dashboard;
